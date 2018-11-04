5ee03cd791a6ca3dbb0a64c680cda05a0926e07d
ayaya <aya@ayatec.jp>
2018-11-05 03:01:23 +0900

    Add style to LinkIcon

--- src/js/components/LinkIcon.js
+++ src/js/components/LinkIcon.js
@@ -1,20 +1,59 @@
 
 // React
 import React from 'react';
+import styled from 'styled-components';
 
 const LinkIcon = ({ link, name, image }) => {
   return (
-    <div>
-      <a href={link}>
-        <div>
-          <img src={image} alt={name} />
-        </div>
-        <div>
-          <div>{name}</div>
-        </div>
-      </a>
-    </div>
+    <Style>
+      <Link href={link}>
+        <Image src={image} alt={name} />
+        <Text>{name}</Text>
+      </Link>
+    </Style>
   );
 };
+
+const Style = styled.div`
+  margin: 1rem;
+  width: 128px;
+  height: 157px;
+  padding: 1rem;
+  border-bottom: .5rem solid transparent;
+
+  transition: all .2s 0s ease;
+  &:hover {
+    -webkit-transform: scale(1.05);
+    border-bottom: .5rem solid #1e3c78;
+  }
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  &:link, &:visited {
+    color: #1e3c78;
+  }
+`;
+
+const Image = styled.img`
+  width: 128px;
+  box-sizing: border-box;
+  overflow: hidden;
+
+  transition: all .2s 0s ease;
+  &:hover {
+    -webkit-transform: scale(1.1);
+  }
+`;
+
+const Text = styled.div`
+  margin-top: .25rem;
+  text-align: center;
+  transition: all .2s 0s ease;
+  &:hover {
+    color: #F7F9FD;
+    text-shadow: 1px 0 0 #1e3c78, -1px 0 0 #1e3c78, 0 2px 0 #1e3c78, 0 -1px 0 #1e3c78;
+  }
+`;
 
 export default LinkIcon;
