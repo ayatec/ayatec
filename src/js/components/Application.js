361d521311baf2c183c707419b060d55c83c0f57
ayaya <aya@ayatec.jp>
2018-11-05 01:26:14 +0900

    Update Application

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -1,17 +1,22 @@
 
 // React
 import React from 'react';
+import LinkIconLibrary from './LinkIconLibrary';
+
+// Image
+import LogoImage from '../../favicon.svg';
 
 class Application extends React.Component {
   render() {
     return (
       <div id="app">
         <header>
-          <img alt="ayatec" src={require('../../favicon.svg')} />
+          <img alt="ayatec" src={LogoImage} />
           <span id="header-title">ayatec</span>
         </header>
         <main>
           main
+          <LinkIconLibrary />
         </main>
         <footer>
           <span id="footer-text">powered by </span>
