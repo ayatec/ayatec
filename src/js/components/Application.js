a8bc00cab9956bcc53d2db13292674f782b567d9
ayaya <aya@ayatec.jp>
2018-11-05 03:01:44 +0900

    Add style to App

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -1,6 +1,9 @@
 
 // React
 import React from 'react';
+import styled from 'styled-components';
+
+// App
 import LinkIconLibrary from './LinkIconLibrary';
 
 // Image
@@ -9,22 +12,69 @@ import LogoImage from '../../favicon.svg';
 class Application extends React.Component {
   render() {
     return (
-      <div id="app">
-        <header>
-          <img alt="ayatec" src={LogoImage} />
-          <span id="header-title">ayatec</span>
-        </header>
-        <main>
-          main
+      <Style>
+        <Header>
+          <HeaderImg alt="ayatec" src={LogoImage} />
+          <HeaderTitle>ayatec</HeaderTitle>
+        </Header>
+        <Main>
           <LinkIconLibrary />
-        </main>
-        <footer>
-          <span id="footer-text">powered by </span>
-          <a href="/">ayatec</a>
-        </footer>
-      </div>
+        </Main>
+        <Footer>
+          <FooterText>powered by </FooterText>
+          <FooterLink href="/">ayatec</FooterLink>
+        </Footer>
+      </Style>
     );
   }
 }
+
+// Style
+const Style = styled.div`
+  color: #1e3c78;
+  font-weight: 900;
+`;
+
+const Header = styled.header`
+  margin: 2rem;
+  text-align: left;
+`;
+
+const HeaderImg = styled.img`
+  width: 128px;
+`;
+
+const HeaderTitle = styled.span`
+  display: none;
+`;
+
+const Main = styled.main`
+  margin: 0 2rem;
+  background: #fff;
+  border-right: .05rem solid #1e3c78;
+  border-left: .05rem solid #1e3c78;
+`;
+
+const Footer = styled.footer`
+  margin: 2rem;
+  text-align: right;
+`;
+
+const FooterText = styled.span`
+`;
+
+const FooterLink = styled.a`
+  text-decoration: none;
+  &:link, &:visited {
+    color: #F7F9FD;
+    text-shadow: 1px 0 0 #1e3c78, -1px 0 0 #1e3c78, 0 1px 0 #1e3c78, 0 -1px 0 #1e3c78;
+  }
+
+  transition: all .2s 0s ease;
+  &:hover {
+    color: #630B39;
+    text-shadow: none;
+  }
+`;
 
 export default Application;
